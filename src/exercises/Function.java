609524561b5680e48c9ee3dff1e45998fb6e17ae
Roma <romadhoke@technology-trend.com>
2019-10-31 11:28:16 +0000

    function java

---
+++ src/exercises/Function.java
@@ -0,0 +1,16 @@
+package exercises;
+
+public class Function
+{
+  public static void main(String[]args)
+{
+    int s1=(5+9);
+    int s2=(6+8);
+System.out.println("sum is"+s1);
+System.out.println("sum is"+s2);
+
+}
+
+
+
+}
