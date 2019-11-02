d9599530644b4f10c1926b8eccdf2f500bd40ef2
Roma <romadhoke@technology-trend.com>
2019-11-02 17:44:05 +0000

    Even odd number

---
+++ src/exercises/EvenOdd.java
@@ -0,0 +1,33 @@
+package exercises;
+
+
+
+public class EvenOdd
+{
+ public static void main(String[]args)
+
+ {
+     if(4%2==0)
+
+       System.out.println("4 is even number");
+       else
+    System.out.println("4 is odd number");
+       if(5%2==0)
+
+           System.out.println("5 is even number");
+           else
+            System.out.println("5 is odd number");
+
+       }
+
+       }
+
+
+
+
+
+
+
+
+
+
