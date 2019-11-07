fc1cb359b016a805dfaa0e19d8489694ff4c7254
Roma <romadhoke@technology-trend.com>
2019-11-07 23:15:28 +0000

    CorrectFunction

---
+++ src/exercises/CorrectFunction.java
@@ -0,0 +1,41 @@
+package exercises;
+
+
+
+public class CorrectFunction
+{
+ public static void main(String[]args){
+
+    correct();
+    Incorrect();
+ }
+ public static void correct()
+ {
+  System.out.println("correct.Great job");
+ }
+ public static void Incorrect()
+ {
+  System.out.println("Incorrect.Bad job");
+ }
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
